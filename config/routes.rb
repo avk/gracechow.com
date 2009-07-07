b56ba5f929e74c4529377778081fb4d65d5a58fe
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-07-07 15:56:41 -0700

    temporarily disabling map.root

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "galleries", :action => "show", :id => Gallery.ordered.first.id
+  # map.root :controller => "galleries", :action => "show", :id => Gallery.ordered.first.id
 
   # See how all your routes lay out with "rake routes"
 
