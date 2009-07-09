9fb859957237be6bf9c09f8d0ab1f3e76184b776
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-07-09 13:14:06 -0700

    making sure the current tab and gallery wrapper are activated for anything gallery-related

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def viewing_gallery?(gallery)
+    gallery_related? and 
+    (
+      current_page?(:id => gallery.id) or 
+      current_page?(:gallery_id => gallery.id) 
+    )
+  end
+  
+  def gallery_related?
+    request.request_uri.match /\/galleries\//i or
+    request.request_uri.match /^\/$/ # map.root or root_path
+  end
+  
 end
