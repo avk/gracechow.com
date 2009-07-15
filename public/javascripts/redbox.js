85d8a893e81c15e2deaf9c50b541ec637b1ffbf6
Arthur Klepchukov <arthur.klepchukov@gmail.com>
2009-07-15 14:51:20 -0700

    hoverboxes now appear centered in the page

--- public/javascripts/redbox.js
+++ public/javascripts/redbox.js
@@ -75,14 +75,14 @@ var RedBox = {
     $("RB_window").style['width'] = 'auto';
     $("RB_window").style['height'] = 'auto';
 
-    // var dimensions = Element.getDimensions($("RB_window"));
-    // var width = dimensions.width;
-    // var height = dimensions.height;
-    // 
-    // $("RB_window").style['left'] = ((pagesize[0] - width)/2) + "px";
+    var dimensions = Element.getDimensions($("RB_window"));
+    var width = dimensions.width;
+    var height = dimensions.height;
+    
+    $("RB_window").style['left'] = ((pagesize[0] - width)/2) + "px";
     // $("RB_window").style['top'] = ((pagesize[1] - height)/2) + "px";
     
-    $("RB_window").style['left'] = '50px';
+    // $("RB_window").style['left'] = '50px';
     $("RB_window").style['top'] = '50px';
   },
 
